23be6ad90b5c245e4013cf83bc3fe19bca36d68e
huang <116301570@qq.com>
2019-10-24 19:39:16 +0800

    no message

--- cmusic/src/router/index.js
+++ cmusic/src/router/index.js
@@ -1,26 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import search from '@/components/search/search.vue'
-import $ from 'jquery'
-import SongSheet from '../components/list/SongSheet.vue'
-import singer from '../components/list/singer.vue'
-import CollectionLately from '../components/list/CollectionLately.vue'
-import play from '../components/list/play.vue'
+import Index from "@/components/index/index"
+import Navitem1 from "@/components/index/navitem/navitem1"
+import Navitem2 from "@/components/index/navitem/navitem2"
+import Navitem3 from "@/components/index/navitem/navitem3"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path:'/search',component:search},
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {path:'/SongSheet',component:SongSheet},
-    {path:'/singer',component:singer},
-    {path:'/CollectionLately',component:CollectionLately},
-    {path:'/play',component:play},
+    {path: '/',redirect: { name: 'index' }},
+    {path: '/index',name: 'index',component: Index},
+    {path: '/navitem1',name: 'Navitem1',component: Navitem1},
+    {path: '/navitem2',name: 'Navitem2',component: Navitem2},
+    {path: '/navitem3',name: 'Navitem3',component: Navitem3},
   ]
 })
